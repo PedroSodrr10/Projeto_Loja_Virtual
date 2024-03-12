1381e4e86865bb9a11ffe0a6f3ad8d8e9a16d392
Pedro Sodré <pedro.0405@hotmail.com>
2024-03-12 15:25:02 -0300

    Commit7

--- Source/script.js
+++ Source/script.js
@@ -1,7 +1,14 @@
 let produtoscarrinho = "Adicionado: "
+let separador = true
 
 function adicionarproduto(nomeproduto) {
-    produtoscarrinho = produtoscarrinho + ", " + nomeproduto;
+    if (separador) {
+        produtoscarrinho = produtoscarrinho + nomeproduto;
+    }
+    if (separador === false) {
+        produtoscarrinho = produtoscarrinho + ", " + nomeproduto;
+    } 
+    separador = false;
     document.getElementById("sacola").innerHTML= produtoscarrinho 
     alert(nomeproduto + " foi adicionado ao carrinho!")
 } 
