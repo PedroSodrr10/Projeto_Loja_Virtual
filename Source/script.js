6e6e76b47e7d781ac793010b705adf3fa3d6cd17
Pedro Sodré <pedro.0405@hotmail.com>
2024-03-20 17:50:00 -0300

    Commit8

--- Source/script.js
+++ Source/script.js
@@ -1,14 +1,50 @@
 let produtoscarrinho = "Adicionado: "
 let separador = true
+let contador = true
+
+function CriarListaProdutos(produto) {
+    if (separador) {
+        produtoscarrinho = produtoscarrinho + produto;
+    } else {
+        produtoscarrinho = produtoscarrinho + ", " + produto;
+    }
+    separador = false;
+}
+
+function AdcProdSacola() {
+    document.getElementById("sacola").innerHTML = produtoscarrinho
+}
+
+function MensagemConfirma() {
+    if (contador) {
+        alert("Seu produto foi adicionado ao carrinho!")
+    }
+    contador = false
+}
 
 function adicionarproduto(nomeproduto) {
-    if (separador) {
-        produtoscarrinho = produtoscarrinho + nomeproduto;
+    CriarListaProdutos(nomeproduto)
+    AdcProdSacola()
+    MensagemConfirma()
+}
+
+function MensagemBemVindo() {
+    alert("Seja Bem vindo(a) à Digital Games PlayStation!")
+}
+
+var botaoMidiaFisica = document.getElementById("btnFisica");
+var botaoMidiaDigital = document.getElementById("btnDigital");
+
+function MidiaSelecionada(event) {
+    var botaoClicado = event.target;
+    if (botaoClicado === botaoMidiaFisica) {
+        botaoMidiaFisica.classList.add("btnMidiaSelec");
+        botaoMidiaDigital.classList.remove("btnMidiaSelec");
+    } else if (botaoClicado === botaoMidiaDigital) {
+        botaoMidiaDigital.classList.add("btnMidiaSelec");
+        botaoMidiaFisica.classList.remove("btnMidiaSelec");
     }
-    if (separador === false) {
-        produtoscarrinho = produtoscarrinho + ", " + nomeproduto;
-    } 
-    separador = false;
-    document.getElementById("sacola").innerHTML= produtoscarrinho 
-    alert(nomeproduto + " foi adicionado ao carrinho!")
-} 
+}
+
+botaoMidiaFisica.addEventListener("click", MidiaSelecionada);
+botaoMidiaDigital.addEventListener("click", MidiaSelecionada);
